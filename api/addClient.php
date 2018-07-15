295d8e0fef97731d929a2cdd9add84eff011b819
Maxim Prosvirkin <Nort359@gmail.com>
2018-07-15 15:01:48 +0300

    Changed api

--- api/addClient.php
+++ api/addClient.php
@@ -2,7 +2,7 @@
 
     // Запись клиента в БД посредством AJAX
 	if(!empty($_POST['userName']) && !empty($_POST['userEmail']) && !empty($_POST['userPhone'])) {
-		echo "Круть!";
+		echo "Ok";
 	} else {
-		echo "Лох штоле?";
+		echo "Что-то пошло не так";
 	}
